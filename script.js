04cc0cefae4fbcd055585c8c871879cf7bc36e38
Adam Black <sinegamy11@gmail.com>
2020-06-15 20:43:59 +0300

    fixed

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 'use strict';
 
-const numberGuess = (number) => {
+const numberGuess = () => {
+
+	let randomFunc = () => {
+		return Math.floor(Math.random() * 100 + 1);
+	}
 
 	const isNumber = (a) => {
 		return !isNaN(parseInt(a)) && isFinite(a);
 	}
 
-	const compareNumber = (number) => {
-		const a = prompt('Угадай число от 1 до 100', '');
+	return () => {
+		let number = randomFunc();
+		let a = prompt('Угадай число от 1 до 100', '');
 
 		if(+a === number) {
-			alert('Угадал!');
+			alert('Поздравляю, вы угадали!');
 			return true;
 		} else if(!a) {
 			alert('game over!')
 			return false;
 		} else if(a > number) {
 			alert('Загаданное число меньше');
-			numberGuess(number);
+			numberGuess()();
 		} else if(a < number) {
 			alert('Загаданное число больше');
-			numberGuess(number);
+			numberGuess()();
 		} else if(!isNumber(a)){
 			alert('Введи число!');
-			numberGuess(number);
+			numberGuess()();
 		}
 	}
-	compareNumber(number)
 }
 
-numberGuess(6)
+let compaireNumber = numberGuess();
+compaireNumber();
+console.dir(compaireNumber);
