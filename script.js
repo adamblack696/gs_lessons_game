534913d971f5c808acb57a8af5026ea1ff10088a
Adam Black <sinegamy11@gmail.com>
2020-06-16 18:52:31 +0300

    add lesson06max

--- script.js
+++ script.js
@@ -1,32 +1,72 @@
 'use strict';
 
-const numberGuess = (number) => {
+let randomFunc = () => {
+	return Math.floor(Math.random() * 100 + 1);
+}
+
+
+const numberGuess = () => {
+	
+
+	let number = randomFunc();
+	console.log(number);
+	let iteration = 10;
 
 	const isNumber = (a) => {
 		return !isNaN(parseInt(a)) && isFinite(a);
 	}
 
-	const compareNumber = (number) => {
-		const a = prompt('Угадай число от 1 до 100', '');
-
-		if(+a === number) {
-			alert('Угадал!');
-			return true;
-		} else if(!a) {
-			alert('game over!')
+	const restartFunc = (message) => { 
+		let restart = confirm(message);
+		if(restart) {
+			number = randomFunc();
+			console.log(number);
+			iteration = 10;
+			return compaireNumber();
+		} else {
 			return false;
-		} else if(a > number) {
-			alert('Загаданное число меньше');
-			numberGuess(number);
-		} else if(a < number) {
-			alert('Загаданное число больше');
-			numberGuess(number);
-		} else if(!isNumber(a)){
-			alert('Введи число!');
-			numberGuess(number);
 		}
 	}
-	compareNumber(number)
+
+	const iterationFunc = () => {
+		if(iteration === 0) {
+			restartFunc('Попытки закончились, хотели бы сыграть еще раз?');
+			return false;
+		} else if(iteration === 'win') {
+			restartFunc('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+			return false;
+		} 
+	}
+
+	return () => {
+		let a = prompt('Угадай число от 1 до 100', '');
+
+		if(+a === number) {
+			iteration = 'win';
+			iterationFunc();
+			return true;
+		} else if(!a) {
+			alert('game over!');
+			return false;
+		} else if(a > number) {
+			iteration--;
+			iterationFunc();
+			alert('Загаданное число меньше, осталось попыток ' + iteration);
+			compaireNumber();
+			return false;
+		} else if(a < number) {
+			iteration--;
+			iterationFunc();
+			alert('Загаданное число больше, осталось попыток ' + iteration);
+			compaireNumber();
+			return false;
+		} else if(!isNumber(a)){
+			alert('Введи число!');
+			compaireNumber();
+		}
+	}
 }
 
-numberGuess(6)
+let compaireNumber = numberGuess();
+compaireNumber();
+console.dir(compaireNumber);
